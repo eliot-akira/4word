8bb630292fb7892e19bd58aec4e3efdb997ce5ee
eliot-akira <me@eliotakira.com>
2017-12-20 20:40:16 +0100

    Template loader: only load index

--- wp-includes/template-loader.php
+++ wp-includes/template-loader.php
@@ -45,27 +45,28 @@ elseif ( is_trackback() ) :
 endif;
 
 if ( defined( 'WP_USE_THEMES' ) && WP_USE_THEMES ) :
-	$template = false;
-	if /*( is_embed() && $template = get_embed_template() ) :
+	/*$template = false;
+	if ( is_embed() && $template = get_embed_template() ) :
 	elseif ( is_404() && $template = get_404_template() ) :
 	elseif ( is_search() && $template = get_search_template() ) :
 	elseif ( is_front_page() && $template = get_front_page_template() ) :
-	elseif*/ ( is_home() && $template = get_home_template() ) :
-	/*elseif ( is_post_type_archive() && $template = get_post_type_archive_template() ) :
+	elseif ( is_home() && $template = get_home_template() ) :
+	elseif ( is_post_type_archive() && $template = get_post_type_archive_template() ) :
 	elseif ( is_tax() && $template = get_taxonomy_template() ) :
 	elseif ( is_attachment() && $template = get_attachment_template() ) :
 		remove_filter( 'the_content', 'prepend_attachment' );
-	elseif ( is_single() && $template = get_single_template() ) :*/
+	elseif ( is_single() && $template = get_single_template() ) :
 	elseif ( is_page() && $template = get_page_template() ) :
 	elseif ( is_singular() && $template = get_singular_template() ) :
-	/*elseif ( is_category() && $template = get_category_template() ) :
+	elseif ( is_category() && $template = get_category_template() ) :
 	elseif ( is_tag() && $template = get_tag_template() ) :
 	elseif ( is_author() && $template = get_author_template() ) :
 	elseif ( is_date() && $template = get_date_template() ) :
-	elseif ( is_archive() && $template = get_archive_template() ) : */
+	elseif ( is_archive() && $template = get_archive_template() ) :
 	else :
 		$template = get_index_template();
-	endif;
+	endif;*/
+	$template = get_index_template();
 
 	/**
 	 * Filters the path of the current template before including it.
